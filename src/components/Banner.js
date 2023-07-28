6394da68f002baf340cd2e20ce8779177042a601
g0-0py <brendan@bsherry.io>
2023-07-28 15:34:31 -0500

    initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+const Banner = () => {
+    return (
+        <div>
+            <h1>Orbit Report</h1>
+            <p> Click Buttons if You Dare </p>
+        </div>
+    )
+}
+
+export default Banner
