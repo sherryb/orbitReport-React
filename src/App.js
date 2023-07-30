1935dc3a0c722c1e8a0ff8f23bc46fc6800917e9
g0-0py <brendan@bsherry.io>
2023-07-30 17:09:30 -0500

    fixing imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import useState from "react"
+import React, { useState } from 'react';
 
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
@@ -10,6 +10,13 @@ function App() {
 
   const [sat, setSat] = useState(satData)
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats)
+  }
 
   return (
     <div>
