6130f10ad16eef74b18c3f3077ec79fc7edac752
g0-0py <brendan@bsherry.io>
2023-08-07 22:55:22 -0500

    css updates

--- src/components/SatImage.js
+++ src/components/SatImage.js
@@ -1,6 +1,5 @@
-const url = "https://www.methanesat.org/_app/immutable/assets/Satellite-91ce2e3c.webp"
-
 function SatelliteImage() {
+    const url = "https://www.methanesat.org/_app/immutable/assets/Satellite-91ce2e3c.webp"
     return (
         <div className="sat-image">
             <img src={url} alt="image of a satellite"></img>
